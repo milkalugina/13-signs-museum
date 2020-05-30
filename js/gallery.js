3bc8adc921527d894a5e089b94ada4eea255a85c
Mila Kalugina <milkalugin@gmail.com>
2020-05-30 20:24:49 +0300

    добавила анимацию ко всем постерам

--- js/gallery.js
+++ js/gallery.js
@@ -4,3 +4,143 @@ $(document).ready(function() {
     $(".back_01").toggleClass("back__01");
   });
 });
+
+$(document).ready(function() {
+  $(".poster_02").click(function() {
+    $(".front_02").toggleClass("front__02");
+    $(".back_02").toggleClass("back__02");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_03").click(function() {
+    $(".front_03").toggleClass("front__03");
+    $(".back_03").toggleClass("back__03");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_04").click(function() {
+    $(".front_04").toggleClass("front__04");
+    $(".back_04").toggleClass("back__04");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_05").click(function() {
+    $(".front_05").toggleClass("front__05");
+    $(".back_05").toggleClass("back__05");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_06").click(function() {
+    $(".front_06").toggleClass("front__06");
+    $(".back_06").toggleClass("back__06");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_07").click(function() {
+    $(".front_07").toggleClass("front__07");
+    $(".back_07").toggleClass("back__07");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_08").click(function() {
+    $(".front_08").toggleClass("front__08");
+    $(".back_08").toggleClass("back__08");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_09").click(function() {
+    $(".front_09").toggleClass("front__09");
+    $(".back_09").toggleClass("back__09");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_10").click(function() {
+    $(".front_10").toggleClass("front__10");
+    $(".back_10").toggleClass("back__10");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_11").click(function() {
+    $(".front_11").toggleClass("front__11");
+    $(".back_11").toggleClass("back__11");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_12").click(function() {
+    $(".front_12").toggleClass("front__12");
+    $(".back_12").toggleClass("back__12");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_13").click(function() {
+    $(".front_13").toggleClass("front__13");
+    $(".back_13").toggleClass("back__13");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_14").click(function() {
+    $(".front_14").toggleClass("front__14");
+    $(".back_14").toggleClass("back__14");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_15").click(function() {
+    $(".front_15").toggleClass("front__15");
+    $(".back_15").toggleClass("back__15");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_16").click(function() {
+    $(".front_16").toggleClass("front__16");
+    $(".back_16").toggleClass("back__16");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_17").click(function() {
+    $(".front_17").toggleClass("front__17");
+    $(".back_17").toggleClass("back__17");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_18").click(function() {
+    $(".front_18").toggleClass("front__18");
+    $(".back_18").toggleClass("back__18");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_19").click(function() {
+    $(".front_19").toggleClass("front__19");
+    $(".back_19").toggleClass("back__19");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_20").click(function() {
+    $(".front_20").toggleClass("front__20");
+    $(".back_20").toggleClass("back__20");
+  });
+});
+
+$(document).ready(function() {
+  $(".poster_21").click(function() {
+    $(".front_21").toggleClass("front__21");
+    $(".back_21").toggleClass("back__21");
+  });
+});
