c4c676bc9bfc5d20d02ebef67846b9b38064122a
Mila Kalugina <milkalugin@gmail.com>
2020-06-18 06:53:34 +0300

    анимация на странице музея

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $(".omen-01").click(function() {
+    $(".stars-svg").toggleClass("animation");
+    $(".stars-moon").toggleClass("animation");
+    $(".stars-big").toggleClass("animation");
+  });
+});
+
+$(document).ready(function() {
+  $(".omen-02").click(function() {
+    $(".glass-1").toggleClass("animation");
+    $(".glass-2").toggleClass("animation");
+  });
+});
+
+$(document).ready(function() {
+  $(".omen-03").click(function() {
+    $(".glass-1").toggleClass("animation");
+    $(".glass-2").toggleClass("animation");
+  });
+});
---
+++ js/museum.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+  $(".friday-room").click(function() {
+    $(".room-friday").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".13-room").click(function() {
+    $(".room-13").toggleClass("ani");
+  });
+});
+$(document).ready(function() {
+  $(".bag-room").click(function() {
+    $(".room-bags").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".bird-room").click(function() {
+    $(".room-bird").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".cafe-room").click(function() {
+    $(".room-cafe").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".clock-room").click(function() {
+    $(".room-clock").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".moon-room").click(function() {
+    $(".room-moon").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".pin-room").click(function() {
+    $(".room-pin").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".plate-room").click(function() {
+    $(".room-plate").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".rubbish-room").click(function() {
+    $(".room-rubbish").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".salt-room").click(function() {
+    $(".room-salt").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".stairs-room").click(function() {
+    $(".room-stairs").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".sweater-room").click(function() {
+    $(".room-sweater").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".tickets-room").click(function() {
+    $(".room-ticket").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".cat-room").click(function() {
+    $(".room-cats").toggleClass("ani");
+  });
+});
+
+$(document).ready(function() {
+  $(".glass-room").click(function() {
+    $(".room-glass").toggleClass("ani");
+  });
+});
