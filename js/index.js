934393d7d80d2a5d0269b60b9e1f7182f29449d9
Mila Kalugina <milkalugin@gmail.com>
2020-06-18 08:55:48 +0300

    доработала 3 анимацию на главной странице

--- js/index.js
+++ js/index.js
@@ -15,7 +15,9 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".omen-03").click(function() {
-    $(".glass-1").toggleClass("animation");
-    $(".glass-2").toggleClass("animation");
+    $(".clock-1").toggleClass("animation");
+    $(".clock-2").toggleClass("animation");
+    $(".clock-3").toggleClass("animation");
+    $(".clock-4").toggleClass("animation");
   });
 });
